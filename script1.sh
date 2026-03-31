bdb63aa0a7d54def4d9e14bbc63e01f3250b0468
Ashutosh4718 <ashutosh.shrivastava122@gmail.com>
2026-03-31 22:31:43 +0530

    Added script1: System Identity Report

---
+++ script1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Script 1: System Identity Report
+# Author: Ashutosh
+
+STUDENT_NAME="Ashutosh"
+SOFTWARE_CHOICE="Python"
+
+KERNEL=$(uname -r)
+USER_NAME=$(whoami)
+UPTIME=$(uptime -p)
+DATE=$(date)
+
+echo "=============================="
+echo " Open Source Audit - $STUDENT_NAME"
+echo "=============================="
+echo "Software : $SOFTWARE_CHOICE"
+echo "Kernel   : $KERNEL"
+echo "User     : $USER_NAME"
+echo "Uptime   : $UPTIME"
+echo "Date     : $DATE"
+echo "License  : GPL (Linux Kernel)"
